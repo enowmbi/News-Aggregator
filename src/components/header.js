50fa0a341768b3dc270309c60e8e7c6f4fc9a87e
Enow Mbi <benowmbi@yahoo.com>
2019-08-24 11:21:06 +0100

    Modify header -added select

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,13 @@ const Header=(props)=>{
 
     return(
         <div className="jumbotron jumbotron">
-            <h2><span className="fas fa-list-alt"> </span> News List</h2>
+            <h2><span className="fa fa-list"> </span> News List</h2>
             <h4>Select News Source </h4>
-            <select id ="sourceNames" className="form-control" onChange="sourceChanged(event)">
-                <option>Please Select News Source </option>          
+            <select id ="sourceNames" className="form-control" onChange = {(event)=>props.newsSourceSelectionChanged(event)} >
+                <option>Please Select News Source </option>
+                {/* <option value="enow">My name is Enow</option> */}
+                {/* <option value="oben">My name is Oben </option> */}
+                {/* <option value="ebai">My name is Ebai </option> */}
             </select>
             <h6 id="sourceDescription">  </h6>
             <div id="sourceUrl">
