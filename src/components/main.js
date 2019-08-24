1f387b821a6c54b21c52b8e30c73a2e175d5b11a
Enow Mbi <benowmbi@yahoo.com>
2019-08-24 11:24:40 +0100

    Modify main.js - added articles

--- src/components/main.js
+++ src/components/main.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Main = (props) =>{
     return(
+        <React.Fragment>
             <ul id="articlesList" className="media-list">
 
             </ul>
+        </React.Fragment>
     )
 }
 
