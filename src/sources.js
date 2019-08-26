412cf01717b316abdb3490ac34dee3ed7a926880
Enow Mbi <benowmbi@yahoo.com>
2019-08-26 11:36:28 +0100

    Modify source.js

--- src/sources.js
+++ src/sources.js
@@ -1,4 +1,4 @@
-const sources = [
+export const newsSources = [
     {
         id: "abc-news-au",
         name: "ABC News (AU)",
@@ -6,19 +6,21 @@ const sources = [
         url: "http://www.abc.net.au/news",
         category: "general",
         language: "en",
-        country: "au",
-        urlsToLogos: {
-            small: "",
-            medium: "",
-            large: ""
-        },
-        sortBysAvailable: [
-"top"
-        ]
-    }
-]
+        country: "au"
+    },
 
-console.log("this is it")
-export function getNewsSources(){
-    return sources;
+    {
+        id: "ccn",
+        name: "Cable News Network(CNN)",
+        description: "This is the single source of truth. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.",
+        url: "http://www.ccn.com/news",
+        category: "general",
+        language: "en",
+        country: "us"
+    }
+
+];
+
+export function getSources(){
+    return newsSources;
 }
