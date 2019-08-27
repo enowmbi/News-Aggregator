6620d0acee470a22fd4bd7c4bb07cec19d31ac74
Enow Mbi <benowmbi@yahoo.com>
2019-08-27 08:16:30 +0100

    Add article.js to articles

---
+++ src/components/article.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Article =(props)=>{
+    return(
+        <React.Fragment>
+            <div className="media-left">
+                <a href={props.url} target="_blank" rel="noopener noreferrer"><img className="media-object" src={props.urlToImage} alt={props.title}/></a>
+            </div>
+            <div className="media-body">
+                <h6 className="media-heading"><a href={props.url} target="_blank" rel="noopener noreferrer">{props.title}</a></h6>
+                <h6><i>{props.author ? props.author :""}<small>(Published on : {props.publishedAt})</small></i></h6>
+                <p>{props.description}</p>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default Article;
