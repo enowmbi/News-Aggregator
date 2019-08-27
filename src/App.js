c6b01a4636232580247b72885c6f24f0b574725f
Enow Mbi <benowmbi@yahoo.com>
2019-08-27 08:12:59 +0100

    Modify App.js -adding selectedSource and articles to state

--- src/App.js
+++ src/App.js
@@ -3,59 +3,79 @@ import Header from './components/header';
 import Footer from './components/footer';
 import Main from './components/main';
 import './App.css';
-import { newsSources } from './sources';
 import { getSources }  from './sources';
+import {getArticles } from './articles';
 import axios from 'axios';
 
 class App extends Component{
 
     state = {
         newsSources: [],
-        errors: []
+        errors: [],
+        selectedSource: { },
+        newsArticles:[]
     }
 
-    await componentDidMount(){
-        console.log("the component has mounted")
-
-        const url = 'https://newsapi.org/v1/articles?source=' + 154+ '&apiKey=c493e95394d444458f3488052428deab'
+    async componentDidMount(){
+        const url = 'https://newsapi.org/v1/sources?language=en'
         //connect to the api here
         try{
-            const { data: sources } = await axios.get(url)
-            // const { data: sources } = axios.get(url)
+            const { data: sources } = await axios.get(url);
             //grab data and update state with   
-            this.setState({newsSources: sources})
+            this.setState({newsSources: sources});
         }
         catch(ex){
-            const newsSources = getSources()
-            this.setState({newsSources: newsSources})
+            //get sample data - static from sources.js - due to internet connection issues
+            const newsSources = getSources();
+            this.setState({newsSources: newsSources});
             console.log( ex + ": Can't connect to the api end point") 
             // if there are errors update the state
-            this.setState({errors: ex + "Can't connect to the api end point"})
+            this.setState({errors: ex + "Can't connect to the api end point"});
 
         }
     }
 
 
-
-    //componentDidMount(){
-
-    //  console.log("Component did mount")
-    //using axios read from newsapi and populate the newsSources in the state
-    // this.setState({newsSources: getNewsSources()});
-    //  getNewsSources();
-
-    // }
-
     handleNewsSourceSelectionChanged =(e)=>{
-        console.log(`Select has changed for ${e.target.value}`);
+        //change the source and update state 
+        const sources  = [...this.state.newsSources];
+        const userSelectedSource = sources.find(source =>source.id === e.target.value.trim());
+        this.setState({selectedSource: userSelectedSource})
+
+        // display articles from the selected source 
+        this.handleDisplayArticles(e.target)
+    }
+
+    async handleDisplayArticles(source){
+        //get articles from url based on source
+        const url = `https://newsapi.org/v1/articles?source=${source}&apiKey=c493e95394d444458f3488052428deab`
+
+        try{
+            const { data: sources } = await axios.get(url);
+            //grab data and update state with   
+            this.setState({newsArticles: sources});
+        }
+        catch(ex){
+            //get sample data - static from sources.js - due to internet connection issues
+            const newsArticles = getArticles(source.value);
+            this.setState({newsArticles: newsArticles});
+            console.log( ex + ": Can't connect to the api end point") 
+            // if there are errors update the state
+            this.setState({errors: ex + "Can't connect to the api end point"});
+        }
+
     }
 
     render(){
         return(
             <React.Fragment>
                 <div className="container-fluid">
-                    <Header  newsSourceSelectionChanged ={this.handleNewsSourceSelectionChanged}/>
-                    <Main />
+                    <Header  
+                        newsSourceSelectionChanged ={this.handleNewsSourceSelectionChanged} 
+                        sources ={this.state.newsSources} 
+                        selectedSource ={this.state.selectedSource}
+                    />
+                    <Main  articles ={this.state.newsArticles}/>
                     <Footer />
                 </div>
             </React.Fragment>
