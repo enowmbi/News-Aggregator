4bee88d046ef0255adcbc37543f44984f38f8f53
Enow Mbi <benowmbi@yahoo.com>
2019-08-27 08:16:03 +0100

    Add articles to main.js

---
+++ src/articles.js
@@ -0,0 +1,29 @@
+export const newsArticles = [
+{
+id: "cnn",
+name: "Cable News Network (CNN)",
+author: "Matthew Beedham",
+title: "California man laundered $25M through his own DIY Bitcoin ATM and exchange",
+description: "A man from the small town of Westwood in California has pleaded guilty to running an unlicensed Bitcoin exchange and ATM, and laundering money. Kunal Kalra, 25 has been found guilty of owning and operating unlicensed money transmitting business which he used …",
+url: "https://thenextweb.com/hardfork/2019/08/26/california-man-25-million-unlicensed-bitcoin-exchange-atm/",
+urlToImage: "https://img-cdn.tnwcdn.com/image/hardfork?filter_last=1&fit=1280%2C640&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2019%2F08%2Fbtc-bitcoin-cash-wash-laundering-atm-batm-transmit-blockchain-criminal-california.jpg&signature=5b5726a4dfd1c8460e33219f56ea597e",
+publishedAt: "2019-08-26T09:33:30Z",
+content: "A man from the small town of Westwood in California has pleaded guilty to running an unlicensed Bitcoin BTC exchange and ATM, and laundering money.\r\nKunal Kalra, 25 has been found guilty of owning and operating unlicensed money transmitting business which he … [+2131 chars]"
+},
+{
+id: "abc-news-au",
+name: "ABC News (AU)",
+author: "Satoshi Nakaboto",
+title: "Satoshi Nakaboto: 'Bitcoin might be showing itself to be a safe haven'",
+description: "Our robot colleague Satoshi Nakaboto writes about Bitcoin every day. Welcome to another edition of Bitcoin Today, where I, Satoshi Nakaboto, tell you what’s been going on with Bitcoin in the past 24 hours. As Albert Einstein used to say: Buy low, sell…",
+url: "https://thenextweb.com/hardfork/2019/08/26/satoshi-nakaboto-bitcoin-might-be-showing-itself-to-be-a-safe-haven/",
+urlToImage: "https://img-cdn.tnwcdn.com/image/hardfork?filter_last=1&fit=1280%2C640&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2019%2F08%2Fbitcoin_today-header_bitcoin_today.jpg&signature=30221b6a68049cc6bc3b58f3ddb38864",
+publishedAt: "2019-08-26T08:50:11Z",
+content: "Our robot colleague Satoshi Nakaboto writes about Bitcoin BTC every day.\r\nWelcome to another edition of Bitcoin Today, where I, Satoshi Nakaboto, tell you whats been going on with Bitcoin in the past 24 hours. As Albert Einstein used to say: Buy low, … [+2458 chars]"
+}
+];
+
+export function getArticles(source){
+    const articles =  newsArticles.filter(article => (article.id === source))
+    return articles
+}
