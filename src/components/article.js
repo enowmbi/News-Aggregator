dc99f75ab8643100d623dd25a5090b906ccf5dfb
Enow Mbi <benowmbi@yahoo.com>
2019-09-13 08:12:21 +0100

    Modify article.js - align description to image

--- src/components/article.js
+++ src/components/article.js
@@ -5,13 +5,17 @@ const Article =(props)=>{
     return(
         <React.Fragment>
             <hr/>
-            <div className="media-left">
-                <a href={props.url} target="_blank" rel="noopener noreferrer"><img className="media-object" src={props.urlToImage} alt={props.title}/></a>
-            </div>
-            <div className="media-body">
-                <h6 className="media-heading"><a href={props.url} target="_blank" rel="noopener noreferrer">{props.title}</a></h6>
-                <h6><i>{props.author ? props.author :""}<small>(Published on : {props.publishedAt})</small></i></h6>
-                <p>{props.description}</p>
+            <div className="row">
+                <div className="col-md-4">
+                    <a href={props.url} target="_blank" rel="noopener noreferrer"><img className="media-object" src={props.urlToImage} alt={props.title}/></a>
+                </div>
+                <div className ="col-md-8">
+                    <div className="media-body">
+                        <h6 className="media-heading"><a href={props.url} target="_blank" rel="noopener noreferrer">{props.title}</a></h6>
+                        <h6><i>{props.author ? props.author :""}<small>(Published on : {props.publishedAt})</small></i></h6>
+                        <p>{props.description}</p>
+                    </div>
+                </div>
             </div>
         </React.Fragment>
     );
