f6b71b09b565b007c2ba2e04d57a0bab1df0ca61
Enow Mbi <benowmbi@yahoo.com>
2019-09-13 10:52:42 +0100

    Modify source details - removed button - use link for name of news agency

--- src/components/source-details.js
+++ src/components/source-details.js
@@ -5,7 +5,7 @@ const SourceDetails =(props) => {
     return(
         <React.Fragment>
                 <h6 className="my-3" id="sourceDescription"> {props.selectedSource ? props.selectedSource.description :  ""}  </h6>
-                <div id="sourceUrl"> <a className="btn btn-primary" target ="_blank" rel="noopener noreferrer" href={props.selectedSource ? props.selectedSource.url : ""}>{props.selectedSource ? props.selectedSource.url : ""}</a></div>
+                <div id="sourceUrl"><a target ="_blank" rel="noopener noreferrer" href={props.selectedSource ? props.selectedSource.url : ""} >{props.selectedSource ? props.selectedSource.url : ""}</a></div>
         </React.Fragment>
     )
 }
