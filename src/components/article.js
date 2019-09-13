b8c3eb3428b4baa345f0486fed21b26af14aec6a
Enow Mbi <benowmbi@yahoo.com>
2019-09-13 11:28:19 +0100

    Modified articles

--- src/components/article.js
+++ src/components/article.js
@@ -6,10 +6,10 @@ const Article =(props)=>{
         <React.Fragment>
             <hr/>
             <div className="row">
-                <div className="col-md-4">
+                <div className="col-md-2">
                     <a href={props.url} target="_blank" rel="noopener noreferrer"><img className="media-object" src={props.urlToImage} alt={props.title}/></a>
                 </div>
-                <div className ="col-md-8">
+                <div className ="col-md-10">
                     <div className="media-body">
                         <h6 className="media-heading"><a href={props.url} target="_blank" rel="noopener noreferrer">{props.title}</a></h6>
                         <h6><i>{props.author ? props.author :""}<small>(Published on : {props.publishedAt})</small></i></h6>
